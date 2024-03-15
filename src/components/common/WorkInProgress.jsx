211a2debf87bbefb991ea4225b8066e88e182882
khalteck <khalidoyeneye@gmail.com>
2024-03-15 20:28:05 +0100

    feat: work in progress

--- src/components/common/WorkInProgress.jsx
+++ src/components/common/WorkInProgress.jsx
@@ -1,6 +1,6 @@
 const WorkInProgress = () => {
   return (
-    <div className="w-full h-[100px] bg-[#fed7aa] p-10 flex justify-center items-center">
+    <div className="w-full h-[100px] bg-[#ffedd5] p-10 flex justify-center items-center">
       <p className="text-center">Work In Progress! 😪</p>
     </div>
   );
